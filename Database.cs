0a37369a28a7ba851c74d14b0f514a43575a7b17
Stanley Haryoto <stanley.haryoto@gmail.com>
2020-10-16 15:53:02 +0800

    Add project files.

---
+++ Database.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
+
+namespace MySQLConnection
+{
+    class Database
+    {
+        MySqlConnection conn = new MySqlConnection();
+
+        public void Connect(string db, string username, string password)
+        {
+            Console.WriteLine(String.Format("Connecting to MySQL {0}...",db));
+            conn.ConnectionString = $"server=localhost;user={username};database={db};port=3306;password={password}";
+            conn.Open();
+        }
+
+        public void Disconnect()
+        {
+            conn.Close();
+        }
+
+        public void SelectRecord(string tableName, string[] attributes)
+        {
+            try
+            {
+
+                string sql = $"SELECT {string.Join(",", attributes)} FROM {tableName}";
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                if (rdr.HasRows == false)
+                {
+                    Console.WriteLine("No records found");
+                    return;
+                }
+
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr[0] + " / " + rdr[1] + " / " + rdr[2]);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public void InsertRecord(string tableName, string[] attributes, string[] values)
+        {
+            try
+            {
+                string sql = $"INSERT INTO {tableName} ({string.Join(",", attributes)}) VALUES ({String.Format("{0}", string.Join(",", values))})";
+
+                Console.WriteLine(sql);
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                int inserted = cmd.ExecuteNonQuery();
+
+                Console.WriteLine(String.Format("{0} record(s) successfully added", inserted));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace MySQLConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Database db = new Database();
+            db.Connect("accountdb", "root", "Dm2341");
+
+            int choice = 0;
+            do
+            {
+                Console.WriteLine("1. Select All Records");
+                Console.WriteLine("2. Insert A New Record");
+                Console.WriteLine("0. Exit");
+                Console.Write("Please select an option above: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch(choice)
+                {
+                    case 1:
+                        db.SelectRecord("user", new string[] { "username", "password", "date_created" });
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Adding New Record");
+                        Console.Write("username: ");
+                        string username = Console.ReadLine();
+                        Console.Write("password: ");
+                        string password = Console.ReadLine();
+
+                        db.InsertRecord("user", new string[] { "username", "password" }, new string[] { $"'{username}'", $"'{password}'" });
+                        break;
+                }
+
+                Console.WriteLine("");
+
+            } while (choice != 0);
+
+            Console.WriteLine("Done.");
+            db.Disconnect();
+        }
+    }
+}
